5e38d08490f907cceadbde31f9e2000a8a651876
Vincent <vkammerer@gmail.com>
2016-05-18 12:36:42 -0400

    Adds application setup and first main view

---
+++ api/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Adaptation of http://stackoverflow.com/questions/4987551/parse-directory-structure-strings-to-json-using-php
+
+function ReadFolderDirectory($dir,$listDir= array())
+{
+    $listDir = array();
+    if($handler = opendir($dir))
+    {
+        while (($sub = readdir($handler)) !== FALSE)
+        {
+            if ($sub != "." && $sub != ".." && $sub != "Thumb.db")
+            {
+                if(is_file($dir."/".$sub))
+                {
+                    $listDir[] = $sub;
+                } elseif(is_dir($dir."/".$sub)) {
+                    $listDir[][$sub] = ReadFolderDirectory($dir."/".$sub);
+                }
+            }
+        }
+        closedir($handler);
+    }
+    return $listDir;
+}
+
+$config = json_decode(file_get_contents('./config.json'));
+$config->assets = ReadFolderDirectory("../assets")[0];
+
+echo json_encode($config);
+
+?>
