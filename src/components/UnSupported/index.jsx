854b415d038385fd5637bc68831257528e6819d4
Vincent <vkammerer@gmail.com>
2016-10-10 12:22:33 -0400

    Refactors and adds full i18n to Unsupported component

--- src/components/UnSupported/index.jsx
+++ src/components/UnSupported/index.jsx
@@ -2,38 +2,57 @@ import React from 'react';
 import { isDesktop, browserName } from 'tion2/utils/platform';
 import css from './css';
 
-const l = navigator.language === 'fr' ? 'fr' : 'en';
+const language = navigator.language === 'fr' ? 'fr' : 'en';
+const reason = isDesktop ? 'desktop' : 'mobile';
 
-const text = {
+const i18nJsx = {
 	headline: {
 		en: 'We\'re sorry!',
 		fr: 'Nous sommes désolés!',
 	},
+	reason: {
+		desktop: {
+			en: (
+				<div className={css.reason}>
+					<i>The Idea Of North 2.0</i> isn't supported on your version of {browserName}.
+					<br /><br />
+					<a className={css.link} href="http://outdatedbrowser.com/en">Upgrade your browser here</a>.
+				</div>
+			),
+			fr: (
+				<div className={css.reason}>
+					<i>The Idea Of North 2.0</i> n'est pas supporté par votre version de {browserName}.
+					<br /><br />
+					<a className={css.link} href="http://outdatedbrowser.com/en">Mettez votre navigateur à jour ici</a>.
+				</div>
+			),
+		},
+		mobile: {
+			en: (
+				<div className={css.reason}>
+					<i>The Idea Of North 2.0</i> was designed for desktop computers.
+					<br /><br />
+					Come back later to see us!
+				</div>
+			),
+			fr: (
+				<div className={css.reason}>
+					<i>The Idea Of North 2.0</i> a été conçue pour les ordinateurs fixes.
+					<br /><br />
+					Revenez nous voir plus tard!
+				</div>
+			),
+		},
+	},
 };
 
-const MyComponent = () => {
-	const reason = isDesktop
-	? (
-		<div className={css.reason}>
-			<i>The Idea Of North 2.0</i> isn't supported on your version of {browserName}.
-			<br /><br />
-			<a className={css.link} href="http://outdatedbrowser.com/en">Upgrade your browser here</a>.
+const MyComponent = () => (
+	<div className={css.unsupported}>
+		<div className={css.headline}>
+			{i18nJsx.headline[language]}
 		</div>
-	) : (
-		<div className={css.reason}>
-			<i>The Idea Of North 2.0</i> was designed for desktop computers.
-			<br /><br />
-			Come back later to see us!
-		</div>
-	);
-	return (
-		<div className={css.unsupported}>
-			<div className={css.headline}>
-				{text.headline[l]}
-			</div>
-			{reason}
-		</div>
-	);
-};
+		{i18nJsx[reason][language]}
+	</div>
+);
 
 export default MyComponent;
