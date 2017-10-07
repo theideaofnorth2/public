0ffb4548d7172332b6115da14f6fb7a6a9e14416
Vincent <vkammerer@gmail.com>
2017-10-07 19:23:54 -0400

    Zooms out main map view in light mode

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,63 +1,63 @@
 const defaultState = {
-	initialized: false,
-	ready: false,
-	dragging: false,
-	animating: false,
-	centering: false,
-	zoom: 4,
-	zooming: false,
-	level: 'main',
-	center: { lat: 62.536813, lng: -97.445291 },
+  initialized: false,
+  ready: false,
+  dragging: false,
+  animating: false,
+  centering: false,
+  zoom: document.location.search.indexOf('light') !== -1 ? 3 : 4,
+  zooming: false,
+  level: 'main',
+  center: { lat: 62.536813, lng: -97.445291 },
 };
 
 export default function reducer(state = defaultState, action = null) {
-	switch (action.type) {
-		case 'MAP_INITIALIZED':
-			return {
-				...state,
-				initialized: true,
-			};
-		case 'MAP_READY':
-			return {
-				...state,
-				ready: true,
-			};
-		case 'MAP_DRAG_START':
-			return {
-				...state,
-				dragging: true,
-			};
-		case 'MAP_DRAG_END':
-			return {
-				...state,
-				dragging: false,
-			};
-		case 'MAP_CENTER':
-			return {
-				...state,
-				center: action.center,
-				animating: true,
-				centering: true,
-			};
-		case 'MAP_CENTER_FINISHED':
-			return {
-				...state,
-				centering: false,
-			};
-		case 'MAP_ZOOM':
-			return {
-				...state,
-				zooming: true,
-				zoom: action.direction === 'in' ? action.zoom : 4,
-				level: action.direction === 'in' ? 'origin' : 'main',
-			};
-		case 'MAP_ZOOM_FINISHED':
-			return {
-				...state,
-				animating: false,
-				zooming: false,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'MAP_INITIALIZED':
+      return {
+        ...state,
+        initialized: true,
+      };
+    case 'MAP_READY':
+      return {
+        ...state,
+        ready: true,
+      };
+    case 'MAP_DRAG_START':
+      return {
+        ...state,
+        dragging: true,
+      };
+    case 'MAP_DRAG_END':
+      return {
+        ...state,
+        dragging: false,
+      };
+    case 'MAP_CENTER':
+      return {
+        ...state,
+        center: action.center,
+        animating: true,
+        centering: true,
+      };
+    case 'MAP_CENTER_FINISHED':
+      return {
+        ...state,
+        centering: false,
+      };
+    case 'MAP_ZOOM':
+      return {
+        ...state,
+        zooming: true,
+        zoom: action.direction === 'in' ? action.zoom : 4,
+        level: action.direction === 'in' ? 'origin' : 'main',
+      };
+    case 'MAP_ZOOM_FINISHED':
+      return {
+        ...state,
+        animating: false,
+        zooming: false,
+      };
+    default:
+      return state;
+  }
 }
