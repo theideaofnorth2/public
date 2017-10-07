245f2cc2630a6942d981df09b28b779d1bc7f96b
Vincent <vkammerer@gmail.com>
2017-10-07 10:55:26 -0400

    Fixes parsing of catpure params

--- src/components/Mapp/index.jsx
+++ src/components/Mapp/index.jsx
@@ -1,50 +1,53 @@
-import React, { Component } from "react";
-import classnames from "classnames";
-import { connect } from "react-redux";
-import { delay } from "redux-saga";
-import OriginInterviewIcons from "tion2/components/Mapp/OriginInterviewIcons";
-import EggIcons from "tion2/components/Mapp/EggIcons";
-import Distances from "tion2/components/Mapp/Distances";
-import Origins from "tion2/components/Mapp/Origins";
-import Destinations from "tion2/components/Mapp/Destinations";
-import utilsCss from "tion2/components/common/utils";
-import appCss from "tion2/components/App/css";
-import canadaCoords from "./data/canadaCoords";
-import worldCoords from "./data/worldCoords";
-import mainStyles from "./data/mainStyles.json";
-import originStyles from "./data/originStyles.json";
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { delay } from 'redux-saga';
+import OriginInterviewIcons from 'tion2/components/Mapp/OriginInterviewIcons';
+import EggIcons from 'tion2/components/Mapp/EggIcons';
+import Distances from 'tion2/components/Mapp/Distances';
+import Origins from 'tion2/components/Mapp/Origins';
+import Destinations from 'tion2/components/Mapp/Destinations';
+import utilsCss from 'tion2/components/common/utils';
+import appCss from 'tion2/components/App/css';
+import canadaCoords from './data/canadaCoords';
+import worldCoords from './data/worldCoords';
+import mainStyles from './data/mainStyles.json';
+import originStyles from './data/originStyles.json';
 import {
   waitForMapIdle,
   setLayers,
   getMapOptionsFromUrl,
-  areCentersEqual
-} from "./utils";
-import css from "./css";
+  areCentersEqual,
+} from './utils';
+import css from './css';
 
 const polygonOptions = {
   strokeWeight: 0,
-  fillColor: "#000000",
+  fillColor: '#000000',
   fillOpacity: 0.8,
-  paths: [worldCoords, canadaCoords]
+  paths: [worldCoords, canadaCoords],
 };
 
 export class MyComponent extends Component {
   constructor(props) {
     super(props);
-    this.setGmapOptions();
+    this.gmapOptions = this.props.app.isCapture
+      ? getMapOptionsFromUrl()
+      : this.getGmapOptions();
   }
   componentDidMount() {
     this.gmap = new google.maps.Map(this.mapRef, this.gmapOptions);
-    this.props.dispatch({ type: "MAP_INITIALIZED" });
+    this.props.dispatch({ type: 'MAP_INITIALIZED' });
   }
   componentDidUpdate(prevProps) {
+    if (this.props.app.isCapture) return;
     if (!prevProps.map.initialized && this.props.map.initialized) {
-      this.gmap.addListener("dragstart", () => {
-        this.props.dispatch({ type: "MAP_DRAG_START" });
+      this.gmap.addListener('dragstart', () => {
+        this.props.dispatch({ type: 'MAP_DRAG_START' });
       });
-      this.gmap.addListener("dragend", () => {
+      this.gmap.addListener('dragend', () => {
         setTimeout(() => {
-          this.props.dispatch({ type: "MAP_DRAG_END" });
+          this.props.dispatch({ type: 'MAP_DRAG_END' });
         }, 0);
       });
       this.setPolygonAndLayers();
@@ -59,33 +62,22 @@ export class MyComponent extends Component {
       polygon.setMap(this.gmap);
       await waitForMapIdle(this.gmap);
       setLayers(css.layer1, css.layer3);
-      this.props.dispatch({ type: "MAP_READY" });
+      this.props.dispatch({ type: 'MAP_READY' });
       if (this.props.app.isCapture) this.setCaptureMapOptions.call(this);
     }
     asyncFunc.call(this);
   };
-  setCaptureMapOptions = () => {
-    this.gmapOptions = getMapOptionsFromUrl();
-    this.gmap.panTo(this.gmapOptions.center);
-    this.gmap.setOptions({ minZoom: undefined, maxZoom: undefined });
-    this.gmap.setOptions({ zoom: 16 });
-    setTimeout(() => {
-      this.gmap.setOptions(this.gmapOptions);
-    }, 0);
-  };
-  setGmapOptions = () => {
-    this.gmapOptions = {
-      backgroundColor: "transparent",
-      disableDefaultUI: true,
-      zoom: this.props.map.zoom,
-      minZoom: this.props.map.zoom,
-      maxZoom: this.props.map.zoom,
-      styles: this.props.map.styles === "main" ? mainStyles : originStyles,
-      center: this.props.map.center
-    };
-  };
+  getGmapOptions = () => ({
+    backgroundColor: 'transparent',
+    disableDefaultUI: true,
+    zoom: this.props.map.zoom,
+    minZoom: this.props.map.zoom,
+    maxZoom: this.props.map.zoom,
+    styles: this.props.map.styles === 'main' ? mainStyles : originStyles,
+    center: this.props.map.center,
+  });
   setZoomAndStyles = () => {
-    this.setGmapOptions();
+    this.gmapOptions = this.getGmapOptions();
     this.gmap.setOptions({ minZoom: undefined, maxZoom: undefined });
     this.gmap.setOptions(this.gmapOptions);
   };
@@ -96,7 +88,7 @@ export class MyComponent extends Component {
         this.gmap.panTo(this.props.map.center);
         await waitForMapIdle(this.gmap);
       }
-      this.props.dispatch({ type: "MAP_CENTER_FINISHED" });
+      this.props.dispatch({ type: 'MAP_CENTER_FINISHED' });
       this.setZoomAndStyles();
     }
     asyncFunc.call(this);
@@ -105,18 +97,19 @@ export class MyComponent extends Component {
     const mapClass = classnames(css.map, {
       [css.capture]: this.props.app.isCapture,
       [css.visible]: this.props.map.ready,
-      [utilsCss.pointable]: this.props.exploration.mode === "interactive"
+      [utilsCss.pointable]: this.props.exploration.mode === 'interactive',
     });
     return (
       <div className={appCss.mapp} hidden={this.props.map.zooming}>
-        {!this.props.app.isCapture &&
+        {!this.props.app.isCapture && (
           <div className={css.geoPositionedContent}>
             <Distances gmap={this.gmap} />
             <Destinations gmap={this.gmap} />
             <Origins gmap={this.gmap} />
             <OriginInterviewIcons gmap={this.gmap} />
             <EggIcons gmap={this.gmap} />
-          </div>}
+          </div>
+        )}
         <div
           ref={ref => {
             this.mapRef = ref;
@@ -131,7 +124,7 @@ export class MyComponent extends Component {
 const mapStateToProps = state => ({
   exploration: state.exploration,
   app: state.app,
-  map: state.map
+  map: state.map,
 });
 
 export default connect(mapStateToProps)(MyComponent);
--- src/components/Mapp/utils.js
+++ src/components/Mapp/utils.js
@@ -2,43 +2,50 @@ import { SuperPromise } from 'tion2/utils/tools';
 import mainStyles from './data/mainStyles.json';
 
 export const getMapOptionsFromUrl = () => {
-	const params = new window.URLSearchParams(window.location);
-	const zoom = parseInt(params.get('zoom'), 10);
-	return {
-		disableDefaultUI: true,
-		zoom,
-		minZoom: zoom,
-		maxZoom: zoom,
-		center: {
-			lat: parseFloat(params.get('lat')),
-			lng: parseFloat(params.get('lng')),
-		},
-		styles: mainStyles,
-	};
+  const params = new window.URLSearchParams(window.location.search);
+  const zoom = parseInt(params.get('zoom'), 10);
+  return {
+    disableDefaultUI: true,
+    zoom,
+    minZoom: zoom,
+    maxZoom: zoom,
+    center: {
+      lat: parseFloat(params.get('lat')),
+      lng: parseFloat(params.get('lng')),
+    },
+    styles: mainStyles,
+  };
 };
 
 export const waitForMapIdle = map => {
-	const myPromise = new SuperPromise();
-	google.maps.event.addListenerOnce(map, 'idle', () => {
-		myPromise.resolve(map);
-	});
-	return myPromise.promise;
+  const myPromise = new SuperPromise();
+  google.maps.event.addListenerOnce(map, 'idle', () => {
+    myPromise.resolve(map);
+  });
+  return myPromise.promise;
 };
 
-export const getGmContainer = () => document.querySelector('.gm-style > div:nth-child(1)');
+export const getGmContainer = () =>
+  document.querySelector('.gm-style > div:nth-child(1)');
 
 export const setLayers = (layer1Class, layer3Class) => {
-	const layer1 = document.querySelector('.gm-style > div:nth-child(1) > div:nth-child(1)');
-	const layer3 = document.querySelector('.gm-style > div:nth-child(1) > div:nth-child(4)');
-	layer1.classList.add(layer1Class);
-	layer3.classList.add(layer3Class);
+  const layer1 = document.querySelector(
+    '.gm-style > div:nth-child(1) > div:nth-child(1)',
+  );
+  const layer3 = document.querySelector(
+    '.gm-style > div:nth-child(1) > div:nth-child(4)',
+  );
+  layer1.classList.add(layer1Class);
+  layer3.classList.add(layer3Class);
 };
 
 export const areCentersEqual = (propsCenter, LatLng) => {
-	const gmapCenter = {
-		lat: LatLng.lat(),
-		lng: LatLng.lng(),
-	};
-	return Math.abs(propsCenter.lat - gmapCenter.lat) < 0.0001 &&
-		Math.abs(propsCenter.lng - gmapCenter.lng) < 0.0001;
+  const gmapCenter = {
+    lat: LatLng.lat(),
+    lng: LatLng.lng(),
+  };
+  return (
+    Math.abs(propsCenter.lat - gmapCenter.lat) < 0.0001 &&
+    Math.abs(propsCenter.lng - gmapCenter.lng) < 0.0001
+  );
 };
