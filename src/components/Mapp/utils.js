f9e9c9f0099ec0482b1c0179d0a385fb4872be04
Vincent Kammerer <vkammerer@gmail.com>
2018-02-28 12:51:00 -0500

    Fixes volume slider and adapts to Google Maps html structure change

--- src/components/Mapp/utils.js
+++ src/components/Mapp/utils.js
@@ -1,39 +1,39 @@
-import { SuperPromise } from 'tion2/utils/tools';
-import mainStyles from './data/mainStyles.json';
+import { SuperPromise } from "tion2/utils/tools";
+import mainStyles from "./data/mainStyles.json";
 
 export const getMapOptionsFromUrl = () => {
   const params = new window.URLSearchParams(window.location.search);
-  const zoom = parseInt(params.get('zoom'), 10);
+  const zoom = parseInt(params.get("zoom"), 10);
   return {
     disableDefaultUI: true,
     zoom,
     minZoom: zoom,
     maxZoom: zoom,
     center: {
-      lat: parseFloat(params.get('lat')),
-      lng: parseFloat(params.get('lng')),
+      lat: parseFloat(params.get("lat")),
+      lng: parseFloat(params.get("lng"))
     },
-    styles: mainStyles,
+    styles: mainStyles
   };
 };
 
 export const waitForMapIdle = map => {
   const myPromise = new SuperPromise();
-  google.maps.event.addListenerOnce(map, 'idle', () => {
+  google.maps.event.addListenerOnce(map, "idle", () => {
     myPromise.resolve(map);
   });
   return myPromise.promise;
 };
 
 export const getGmContainer = () =>
-  document.querySelector('.gm-style > div:nth-child(1)');
+  document.querySelector(".gm-style > div:nth-child(1)");
 
 export const setLayers = (layer1Class, layer3Class) => {
   const layer1 = document.querySelector(
-    '.gm-style > div:nth-child(1) > div:nth-child(1)',
+    ".gm-style > div:nth-child(1) > div:nth-child(1)"
   );
   const layer3 = document.querySelector(
-    '.gm-style > div:nth-child(1) > div:nth-child(4)',
+    ".gm-style > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)"
   );
   layer1.classList.add(layer1Class);
   layer3.classList.add(layer3Class);
@@ -42,7 +42,7 @@ export const setLayers = (layer1Class, layer3Class) => {
 export const areCentersEqual = (propsCenter, LatLng) => {
   const gmapCenter = {
     lat: LatLng.lat(),
-    lng: LatLng.lng(),
+    lng: LatLng.lng()
   };
   return (
     Math.abs(propsCenter.lat - gmapCenter.lat) < 0.0001 &&
