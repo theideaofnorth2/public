4dade55fc9ca8485bd15efcedc27b8bc70a0e249
Vincent Kammerer <vkammerer@gmail.com>
2018-07-11 16:52:23 -0400

    Adds static version

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -7,8 +7,7 @@ export const SuperPromise = () => {
 };
 const productionDomain = "theideaofnorth2.com";
 
-export const getApiUri = isLight =>
-  `https://${productionDomain}/api/wordpressApi.php${isLight ? "-light" : ""}/`;
+export const getApiUri = () => `https://${productionDomain}/api/`;
 
 export const introductionImageUri =
   "https://theideaofnorth2.com/assets/images/00000000660.jpg";
