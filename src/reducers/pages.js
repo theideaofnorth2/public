e9f261350a0580c200088387e980274be86cfa7f
Vincent Kammerer <vkammerer@gmail.com>
2018-07-12 15:56:47 -0400

    Handles pages content properly

--- src/reducers/pages.js
+++ src/reducers/pages.js
@@ -1,38 +1,46 @@
+import { decodeEntities } from "tion2/utils/tools";
+
 const defaultState = {
-	data: null,
-	selectedPage: null,
+  data: null,
+  selectedPage: null,
 };
 
-const getPages = data => ({
-	approach: data.pages.find(p => p.name === 'approach'),
-	about: data.pages.find(p => p.name === 'about'),
-	tourEnd: data.pages.find(p => p.name === 'tourEnd'),
-});
-
+const getPages = data => {
+  const escapedPages = data.pages.map(p => ({
+    name: p.name,
+    en: decodeEntities(p.en),
+    fr: decodeEntities(p.fr),
+  }));
+  return {
+    approach: escapedPages.find(p => p.name === "approach"),
+    about: escapedPages.find(p => p.name === "about"),
+    tourEnd: escapedPages.find(p => p.name === "tourEnd"),
+  };
+};
 export default function reducer(state = defaultState, action = null) {
-	switch (action.type) {
-		case 'CONFIG_READY': {
-			return {
-				...state,
-				data: getPages(action.data),
-			};
-		}
-		case 'MENU_APPROACH_CLICK':
-			return {
-				...state,
-				selectedPage: 'approach',
-			};
-		case 'MENU_ABOUT_CLICK':
-			return {
-				...state,
-				selectedPage: 'about',
-			};
-		case 'TOUR_END':
-			return {
-				...state,
-				selectedPage: 'tourEnd',
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "CONFIG_READY": {
+      return {
+        ...state,
+        data: getPages(action.data),
+      };
+    }
+    case "MENU_APPROACH_CLICK":
+      return {
+        ...state,
+        selectedPage: "approach",
+      };
+    case "MENU_ABOUT_CLICK":
+      return {
+        ...state,
+        selectedPage: "about",
+      };
+    case "TOUR_END":
+      return {
+        ...state,
+        selectedPage: "tourEnd",
+      };
+    default:
+      return state;
+  }
 }
--- src/utils/tools.js
+++ src/utils/tools.js
@@ -11,3 +11,24 @@ export const getApiUri = () => `https://${productionDomain}/api/`;
 
 export const introductionImageUri =
   "https://theideaofnorth2.com/assets/images/00000000660.jpg";
+
+export const decodeEntities = (() => {
+  // this prevents any overhead from creating the object each time
+  const element = document.createElement("div");
+
+  const decodeHTMLEntities = stri => {
+    let str = stri;
+    if (str && typeof str === "string") {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gim, "");
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gim, "");
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = "";
+    }
+
+    return str;
+  };
+
+  return decodeHTMLEntities;
+})();
